28ec88c0bb1fe2adad009c7b1e010e1dd67d14e5
ken restivo <krestivo@restivo.org>
2012-04-24 12:03:54 -0700

    add github

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject utilza "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ken's random web-related utilities"
+  :url "https://github.com/kenrestivo/utilza"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]])
