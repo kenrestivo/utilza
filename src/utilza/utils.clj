3686a229ccb77a52e2a53ee413abae76900c6757
ken restivo <krestivo@restivo.org>
2012-04-25 09:52:37 -0700

    add server start function

--- src/utilza/utils.clj
+++ src/utilza/utils.clj
@@ -11,10 +11,18 @@
   "Make nice for jquery tabs"
   [m]
   (html [:ul
-       (for [x m]
-         [:li
-          [:a {:href (->> x  :title anchorify (str "#"))} (:title x)]])]
+         (for [x m]
+           [:li
+            [:a {:href (->> x  :title anchorify (str "#"))} (:title x)]])]
         (for [x m]
           [:div {:id (-> x :title anchorify )} (:body x)])))
 
 
+
+;; TODO: make this into a macro, so server/start doesn't have to be pulled in
+(defn manual []
+  (let [mode :dev
+        port 8081]
+    (server/start port
+                  {:mode mode
+                   :ns (-> (.split #"\." (.toString *ns*)) first symbol )})))
