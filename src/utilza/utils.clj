561a30304a983cdf7bfb9fc50c48a53fb71c7338
ken restivo <krestivo@restivo.org>
2012-04-25 19:07:32 -0700

    handle ajax tabs

--- src/utilza/utils.clj
+++ src/utilza/utils.clj
@@ -11,11 +11,13 @@
   "Make nice for jquery tabs"
   [m]
   (html [:ul
-         (for [x m]
-           [:li
-            [:a {:href (->> x  :title anchorify (str "#"))} (:title x)]])]
+       (for [x m]
+         [:li
+          [:a {:href (or (:href x) (->> x  :title anchorify (str "#")))}
+           (:title x)]])]
         (for [x m]
           [:div {:id (-> x :title anchorify )} (:body x)])))
+
 
 
 
