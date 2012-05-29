43ece09400bf916024121076cd0a168382e92c1d
ken restivo <krestivo@restivo.org>
2012-05-29 14:13:51 -0700

    just give me the 404 or whatever, nevermind throwing exceptions.

--- src/utilza/http.clj
+++ src/utilza/http.clj
@@ -23,12 +23,12 @@
      (~f ~@args)))
 
 
-
+ 
 (defmacro spewsave [& body]
   "Give it the function and args, i.e. client/get url, but without parens.
    Saves the :body of the request to /tmp/result.html, and returns the headers."
   `(binding [clj-http.core/*cookie-store* cookies]
-     (let [r# (~@body)]
+     (let [r# (try (~@body) (catch Exception e# (-> e# .getData :object)))]
        (spit "/tmp/result.html" (:body r#))
        (dissoc r# :body))))
 
