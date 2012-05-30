e27b0f39820d764c5614bce0218b8c56997898c2
ken restivo <krestivo@restivo.org>
2012-05-30 12:27:08 -0700

    turn file paths into tree

--- src/utilza/core.clj
+++ src/utilza/core.clj
@@ -1,2 +1,29 @@
+;;; miscellaneous stuff
+
 (ns utilza.core)
 
+
+
+(defn path-to-tree
+  [spl path]
+  (let [tk (-> path (.split spl) seq rest)]
+    [(butlast tk) (last tk)]))
+
+(defn treeify
+  "Turn paths and vals into a tree. Via amalloy"
+  [paths]
+  (reduce
+   (fn [m [path value]]
+     (assoc-in m `(~@path ::self) value))
+   {} paths))
+
+(comment ;;; example
+
+ (treeify (map (partial path-to-tree "/")
+                ["/foo/bar/baz/1",
+                 "/foo/bar/baz/quux/2"]))
+  
+
+
+
+  
