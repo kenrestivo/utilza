aa343a1f83ee1cd8e2855096f279fdc5f706977d
ken restivo <krestivo@restivo.org>
2012-07-31 19:26:44 -0700

    don't make the hairball dependencies transitive!

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/kenrestivo/utilza"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-beta7"]
-                 [clj-http "0.4.1"]
-                 [com.ashafa/clutch "0.3.0"]])
+  :dependencies []
+  :dev-dependencies [
+                     [org.clojure/clojure "1.4.0"]
+                     [noir "1.3.0-beta7"]
+                     [clj-http "0.4.1"]
+                     [com.ashafa/clutch "0.3.0"]])
