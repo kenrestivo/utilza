0c538364bd6c1d91a0370c8efa225a46c2f259b2
ken restivo <ken@restivo.org>
2013-06-09 16:47:56 -0700

    Add no-cookie version of spewsave.

--- src/utilza/http.clj
+++ src/utilza/http.clj
@@ -26,6 +26,8 @@
 
 (defmacro spewsave [& body]
   "Give it the function and args, i.e. client/get url, but without parens.
+   Wraps this in a local cookie store.
+  If there are exceptions, returns them instead of throwing them.
    Saves the :body of the request to /tmp/result.html, and returns the headers."
   `(binding [clj-http.core/*cookie-store* cookies]
      (let [r# (try (~@body) (catch Exception e# (-> e# .getData :object)))]
@@ -33,6 +35,14 @@
        (dissoc r# :body))))
 
 
+(defmacro spew-nocookie [& body]
+  "Give it the function and args, i.e. client/get url, but without parens.
+  Doesnt' do anything special with cookie stores.
+  If there are exceptions, returns them instead of throwing them.
+   Saves the :body of the request to /tmp/result.html, and returns the headers and status."
+  `(let [r# (try ~@body (catch Exception e# (-> e# .getData :object)))]
+     (spit "/tmp/result.html" (:body r#))
+     (dissoc r# :body)))
 
 (defmacro just-gimme
   "Oh, FFS!"
