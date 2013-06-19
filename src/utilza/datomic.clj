217cd50beba37722f1abda8779d739edb7e659e0
ken restivo <ken@restivo.org>
2013-06-19 03:14:31 -0700

    Add a few more datomic utilities

--- src/utilza/datomic.clj
+++ src/utilza/datomic.clj
@@ -15,11 +15,26 @@
        ffirst))
 
 
-
 (defn load-from-file
   "Load and run bunch of transactions from a file"
-  [fname]
+  [fname conn]
   (doseq [tx (-> fname
                  clojure.java.io/reader 
                  Util/readAll)]
-    (t tx)))
+    (d/transact conn  tx)))
+
+
+(defn auto-create
+  "Create mem db's for testing, if the uri is a mem db. Dangerous in production!"
+  [uri]
+  (when (.contains uri "datomic:mem")
+    (d/create-database uri)))
+
+
+
+
+(defn re-seed
+  "Load a seq of files worth of transactions into conn"
+  [files conn]
+  (doseq [f files]
+    (load-from-file f conn)))
