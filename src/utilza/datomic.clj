177026c08d7b7235bc802a8dd12aa2ebdd67a95d
ken restivo <ken@restivo.org>
2013-06-19 03:07:35 -0700

    Add some datomic-related utilities.

---
+++ src/utilza/datomic.clj
@@ -0,0 +1,25 @@
+;; utilities for working with datomic
+
+(ns utilza.datomic
+  (:require [datomic.api :as d])
+  (:import datomic.Util))
+
+
+
+
+(defn fq
+  "Loads the first result"
+  [& args]
+  (->> args
+       (apply d/q)
+       ffirst))
+
+
+
+(defn load-from-file
+  "Load and run bunch of transactions from a file"
+  [fname]
+  (doseq [tx (-> fname
+                 clojure.java.io/reader 
+                 Util/readAll)]
+    (t tx)))
