a9b1baf0ddb3d74e20f4b56de601c61861cab303
ken restivo <ken@restivo.org>
2013-06-20 23:28:49 -0700

    Display a spew of enlive tags without their content.

--- src/utilza/enlive.clj
+++ src/utilza/enlive.clj
@@ -1,5 +1,6 @@
 (ns utilza.enlive
-  (:require clojure.walk))
+  (:require clojure.walk
+            clojure.pprint))
 
 ;; utilities for dealing with enlive tree structures
 
@@ -9,10 +10,24 @@
   [x]
   (if-not (map? x)
     x
-    (update-in x [:content]  (partial filter map?))))
+    (update-in x [:content]  #(if-let [m (filter map? %)]
+                                m
+                                %))))
 
 
 (defn un-html
   "Removes the HTML from the enlive, leaving only the tree structure"
   [tag]
   (clojure.walk/postwalk un-html* tag))
+
+
+
+(defn spew-un-html
+  "Takes a filename to spew to, and an Enlive tag set snippet.
+  Writes a deeply pretty-printed, cleaned tree to the filename"
+  [out-filename tag]
+  (binding [*print-length* 10000 *print-level* 10000]
+    (->> tag
+         un-html
+         (#(with-out-str (clojure.pprint/pprint %)))
+         (spit out-filename))))
