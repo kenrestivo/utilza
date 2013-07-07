db609662a2e3e86bb32102d7b2e7e00aca01fea1
ken restivo <ken@restivo.org>
2013-07-06 20:50:02 -0700

    Version must be bumped again, thanks clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utilza "0.1.21"
+(defproject utilza "0.1.22"
   :description "ken's random web-related utilities"
   :url "https://github.com/kenrestivo/utilza"
   :license {:name "Eclipse Public License"
