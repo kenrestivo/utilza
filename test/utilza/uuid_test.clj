72bf4063c3a0389cd35817065c33c2c56ed1f3a3
ken restivo <ken@restivo.org>
2013-08-17 01:53:16 -0700

    Guard against regression.

--- test/utilza/uuid_test.clj
+++ test/utilza/uuid_test.clj
@@ -3,10 +3,17 @@
         utilza.uuid))
 
 (deftest uuids
-  (testing "UUID roundtrip")
+  (testing "Random UUID roundtrip")
 
   (let [uuid  (java.util.UUID/randomUUID)
         uuids (.toString uuid)]
+    (is (=  uuids
+            (-> uuids
+                uuids->short-url
+                short-url->uuids))))
+
+  (testing "large value UUID roundtrip")
+  (let [uuids "bd711f41-d433-4a2a-9afd-ba2cc855542b"]
     (is (=  uuids
             (-> uuids
                 uuids->short-url
