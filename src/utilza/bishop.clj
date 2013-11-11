35b6618a03afa0f820d45bbf8ae1463d1d45b1ea
ken restivo <ken@restivo.org>
2013-11-11 00:09:41 -0800

    Add a macro to make using bishop less eye-bugging.

---
+++ src/utilza/bishop.clj
@@ -0,0 +1,21 @@
+;; utilities for working with bishop
+
+(ns utilza.bishop
+  (:require [utilza.repl :as urepl]))
+
+
+(defmacro json-get
+  "Short-hand for a get-only, json-only resource. Takes as args a vector of the names
+   of the vars  used to represent the request and params, and a  body to execute."
+  [[req-binding param-binding] body]
+  `{"application/json" (fn [{:keys [request-method ~param-binding] :as ~req-binding}]
+                         (condp = request-method
+                           :get {:body ~body}))})
+
+
+
+(comment
+
+
+
+  )
