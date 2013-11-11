821bb524bfa57c7f88d1420a2f238dcc35cf90cd
ken restivo <ken@restivo.org>
2013-11-11 00:13:14 -0800

    Hygeiene.

--- src/utilza/bishop.clj
+++ src/utilza/bishop.clj
@@ -8,8 +8,8 @@
   "Short-hand for a get-only, json-only resource. Takes as args a vector of the names
    of the vars  used to represent the request and params, and a  body to execute."
   [[req-binding param-binding] body]
-  `{"application/json" (fn [{:keys [request-method ~param-binding] :as ~req-binding}]
-                         (condp = request-method
+  `{"application/json" (fn [{:keys [request-method# ~param-binding] :as ~req-binding}]
+                         (condp = request-method#
                            :get {:body ~body}))})
 
 
