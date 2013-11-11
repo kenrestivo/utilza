ac628fa2ba11c0e9c0b70c00e430bdae6ae4f3e8
ken restivo <ken@restivo.org>
2013-11-11 09:02:51 -0800

    Wrap the whole bishop function, arrgh macros don't compose.

--- src/utilza/bishop.clj
+++ src/utilza/bishop.clj
@@ -4,13 +4,23 @@
   (:require [utilza.repl :as urepl]))
 
 
-(defmacro json-get
-  "Short-hand for a get-only, json-only resource. Takes as args a vector of the names
-   of the vars  used to represent the request and params, and a  body to execute."
-  [[req-binding param-binding] body]
-  `{"application/json" (fn [{:keys [request-method# ~param-binding] :as ~req-binding}]
-                         (condp = request-method#
-                           :get {:body ~body}))})
+
+(defn wrap-get-only
+  [f]
+  (fn [{:keys [request-method params] :as request}]
+    (condp = request-method
+      :get {:body (f request)})))
+
+
+(defmacro def-json-get
+  "Short-hand for a get-only, json-only resource. Takes the resource name,
+    a function for the json body that takes a request as an arg,
+    and any other parameters to bishop.core/defresource"
+  [res-name f & body]
+  `(def ~res-name
+     (com.tnrglobal.bishop.core/resource
+      {"application/json" (wrap-get-only ~f)}
+      ~@body)))
 
 
 
