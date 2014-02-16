5f6b4a7dbde96e03a5b383776ca7ad74621178c0
ken restivo <ken@restivo.org>
2014-02-16 09:33:02 -0800

    Mutability is serious business!

--- src/utilza/memdb.clj
+++ src/utilza/memdb.clj
@@ -3,8 +3,8 @@
   http://www.brandonbloom.name/blog/2013/06/26/slurp-and-spit/
 
   Usage: just swap! the db to save data to memory.
-  To persist to disk, just call (save-data)
-  To read from disk, just (read-data)
+  To persist to disk, just call (save-data!)
+  To read from disk, just (read-data!)
   You can supply args to read/write from those."
   
   (:import java.io.File)
@@ -18,11 +18,11 @@
 
 
 
-(defn save-data
+(defn save-data!
   "With no args, saves to :db-filename saved in env.
    With one arg, saves to the path/filename specified."
   ([]
-     (-> env/env :db-filename save-data))
+     (-> env/env :db-filename save-data!))
   ([dbfilename]
      (let [tmpfile (str dbfilename ".tmp")]
        (send-off save-agent
@@ -31,11 +31,11 @@
                    (.renameTo (File. tmpfile) (File. dbfilename)))))))
 
 
-(defn read-data
+(defn read-data!
   "With no args, reads from :db-filename saved in env.
    With one arg, reads from the path/filename specified."
   ([]
-     (->> env/env :db-filename read-data))
+     (->> env/env :db-filename read-data!))
   ([dbfilename]
      (reset! db (->> dbfilename slurp edn/read-string))))
 
