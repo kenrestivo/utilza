f6eab0f1844e9900a81355fa2e2ffd844701822c
ken restivo <ken@restivo.org>
2014-02-26 22:05:53 -0800

    Move memdb to a ns, split out the file ops.

--- src/utilza/memdb/file.clj
+++ src/utilza/memdb/file.clj
@@ -1,4 +1,4 @@
-(ns utilza.memdb
+(ns utilza.memdb.file
   "A very simple atom-backed memory db mostly lifted from
   http://www.brandonbloom.name/blog/2013/06/26/slurp-and-spit/
 
@@ -43,12 +43,6 @@
      (reset! db (->> dbfilename slurp edn/read-string))))
 
 
-(defn update-record
-  "Returns a function to update the db by applying f to the record at id.
-   Suitable for use with swap!"
-  [id f]
-  (fn [db]
-    (update-in db [id] f)))
 
 
 (defn load []
@@ -57,42 +51,3 @@
     (log/info "Loading db first." (:db-filename env/env))
     (read-data!)
     (log/info "DB loaded (presumably)")))
-
-
-
-(defn all-keys
-  [db k]
-  (reduce #(conj %1 %2)
-          #{}
-          (r/map k (-> db vals))))
-
-(defn rinc
-  [c _]
-  (inc c))
-
-
-(defn key-set-counts
-  "Gets counts for all unique records in db which satisfy key function."
-  [db k]
-  (->> db
-       vals
-       (map k)
-       frequencies
-       (sort-by second)
-       reverse))
-
-
-(defn total-not-null-counts
-  "Finds total count of all records with not-nul key satisfying function k."
-  [db k]
-  (reduce rinc 0 (r/filter k (vals db))))
-
-
-(defn simple-contains
-  [db k s]
-  (->> (reduce #(conj %1 %2)
-               []
-               (->> db
-                    vals
-                    (r/filter #(some-> % k (.contains s)))))
-       (sort-by :time)))
