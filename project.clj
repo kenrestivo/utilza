c5d7cfa789bce1a830c5672357078bd122cd5954
ken restivo <ken@restivo.org>
2014-04-05 11:20:10 -0700

    *sigh* clojars won't let me upload the same version twice.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utilza "0.1.52"
+(defproject utilza "0.1.53"
   :description "ken's random utilities"
   :url "https://github.com/kenrestivo/utilza"
   :license {:name "Eclipse Public License"
