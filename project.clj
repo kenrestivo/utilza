0603ca638355ec6271eab6fdb143593749bcba40
ken restivo <ken@restivo.org>
2014-04-05 11:15:30 -0700

    Doh.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utilza "0.1.52q"
+(defproject utilza "0.1.52"
   :description "ken's random utilities"
   :url "https://github.com/kenrestivo/utilza"
   :license {:name "Eclipse Public License"
