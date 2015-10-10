8ec6f766de6758c64ce2d9d39592f36efaf784ab
ken restivo <ken@restivo.org>
2015-10-10 01:05:30 -0700

    Handle errors, also don't deal with ipv6

--- src/utilza/unix.clj
+++ src/utilza/unix.clj
@@ -2,15 +2,21 @@
   (:require  [me.raynes.conch.low-level :as sh]
              [clojure.java.io :as jio]))
 
+
 (defn get-ip-address
   []
-  (->   (->>  (sh/proc "/bin/ip" "addr" "show" "eth0")
-              :out
-              jio/reader
-              line-seq
-              (filter #(.contains % "inet"))
-              first)
-        (clojure.string/split #" ")
-        (nth 5)
-        (clojure.string/split #"/")
-        first))
+  (try
+    (->   (->>  (sh/proc "/bin/ip" "addr" "show" "eth0")
+                :out
+                jio/reader
+                line-seq
+                (filter #(.contains % "inet"))
+                (remove #(.contains % "inet6"))
+                first)
+          (clojure.string/split #" ")
+          (nth 5)
+          (clojure.string/split #"/")
+          first)
+    (catch Exception e
+      (log/error "can't get ip address")
+      "Network error"))))
