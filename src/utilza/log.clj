e1b04c695b327813bdd10e9d85f0e812aa8c00b9
ken restivo <ken@restivo.org>
2015-11-11 23:00:09 -0800

    Use Timbre

--- src/utilza/log.clj
+++ src/utilza/log.clj
@@ -1,11 +1,13 @@
 (ns utilza.log
   (:require [clj-stacktrace.repl :as cst]
-            [clojure.tools.logging :as log]))
+            [taoensso.timbre :as log]))
 
 
 
-(defn error
-  [data e]
-  {:pre [(isa? (class e) Throwable)
-         (not (isa? (class data) Throwable))]}
-  (log/error (pr-str data) "\n" (with-out-str (cst/pst e))))
+(defmacro catcher 
+  "Executes body within a try/catch, and logs the error using TImbre"
+  [body]
+  `(try
+     ~body
+     (catch Exception e#
+       (log/error e#))))
