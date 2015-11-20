4c56676ee7f5044e88b3bb42bce30610e1b56217
ken restivo <ken@restivo.org>
2015-11-20 01:22:32 -0800

    Add retry backoff function

--- src/utilza/log.clj
+++ src/utilza/log.clj
@@ -11,3 +11,51 @@
      ~body
      (catch Exception e#
        (log/error e#))))
+
+
+(defn try-times-p*
+  [max time f p?]
+  (loop [n 1]
+    (if (< max n)
+      (try
+        (f)
+        (catch Throwable t
+          (log/error t)))
+      (do 
+        (try
+          (f)
+          (catch Throwable t
+            (if (p? t)
+              (do (log/warn (.getMessage t))
+                  (Thread/sleep (* time n)))
+              (log/error t))))
+        (recur (inc n))))))
+
+
+;; ugly, but does the job
+(defn try-times*
+  [max time f]
+  (loop [n 1]
+    (if (< max n)
+      (try
+        (f)
+        (catch Throwable t
+          (log/error t)))
+      (do 
+        (try
+          (f)
+          (catch Throwable t
+            (log/warn (.getMessage t))
+            (Thread/sleep (* time n))))
+        (recur (inc n))))))
+
+
+(defmacro try-times
+  "Retries function with backoff. 
+   Logs warnings on retry, error after retries expired"
+  [times retry-wait & body]
+  `(try-times* 
+    ~times 
+    ~retry-wait
+    (fn [] ~@body)))
+
