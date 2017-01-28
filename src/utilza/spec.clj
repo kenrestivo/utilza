2759ef0183bdfc7b2413a1e3e0f92ce2da438529
ken restivo <ken@restivo.org>
2017-01-28 12:07:12 -0800

    Add spec settings

---
+++ src/utilza/spec.clj
@@ -0,0 +1,12 @@
+(ns utilza.spec
+  [clojure.spec :as s])
+
+(defn validate
+  "Validates settings based on setting-spec supplied"
+  [settings-spec settings]
+  (if (s/valid? settings-spec settings)
+    (s/conform settings-spec settings)
+    (do
+      ;; XXX hack. For some reason, explain doesn't print to stdout in repl, so use println.
+      (println (s/explain-str settings-spec settings))
+      (throw (ex-info "Invalid settings" (s/explain-data settings-spec settings))))))
