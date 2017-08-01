669055d9dc29a41f0706ef5065f08986cd17b082
ken restivo <ken@restivo.org>
2017-07-31 21:59:47 -0700

    Bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject utilza "0.1.92"
+(defproject utilza "0.1.93"
   :description "ken's random utilities"
   :url "https://github.com/kenrestivo/utilza"
   :license {:name "Eclipse Public License"
